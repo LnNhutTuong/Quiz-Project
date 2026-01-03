1f072c6ad9c5fa791a9e5b9491c2c3a30df095f7
tuong <you@example.com>
2026-01-03 16:28:40 +0700

    state and change app arrow function to class

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,12 +1,18 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+  state = {
+    name: "Ximen",
+    age: 20,
+    sex: "Mail",
+  };
+
   //JSX
   render() {
     return (
       <div>
-        My first component
-        {Math.random()}
+        My name is {this.state.name}, I'm {this.state.age} and sex is
+        {this.state.sex};
       </div>
     );
   }
